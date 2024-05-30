17ddfc7af8b9152071eac877808f6fd4fce46610
Dario Lo Forti <lofortidario@gmail.com>
2024-05-30 14:50:45 +0200

    feat: Add createSlug function and tests
    test- 1

---
+++ createSlug.js
@@ -0,0 +1,7 @@
+// createSlug.js
+function createSlug(title, posts) {
+    return title = "Hello World";
+  }
+  
+  module.exports = { createSlug };
+  
---
+++ tests/createSlug.test.js
@@ -0,0 +1,12 @@
+// Eseguiamo i seguenti test:
+const createSlug = require('../createSlug.js');
+// createSlug dovrebbe ritornare una stringa
+test('createSlug dovrebbe ritornare una stringa', () => {
+  const result = createSlug.createSlug('Hello World', []);
+  expect(typeof result).toBe('string');
+});
+// createSlug dovrebbe ritornare una stringa in lowercase
+// createSlug dovrebbe ritornare una stringa con gli spazi sostituiti da -
+// createSlug dovrebbe incrementare di 1 lo slug quando esiste già
+// createSlug dovrebbe lanciare un errore in caso di titolo non presente o formato errato
+// createSlug dovrebbe lanciare un errore se manca l'array dei post
