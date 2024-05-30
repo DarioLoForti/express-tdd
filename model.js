1d089d9d059eb43496840513081c0e41db5dd2f2
Dario Lo Forti <lofortidario@gmail.com>
2024-05-30 15:55:53 +0200

    Add required parameter check to Model constructor

--- model.js
+++ model.js
@@ -1,5 +1,10 @@
 class Model {
-    
+    constructor(fileName) {
+        if (!fileName) {
+          throw new Error('Parametro obbligatorio mancante');
+        }
+        this.fileName = fileName;
+      }
   }
   
   module.exports = Model;
--- tests/model.test.js
+++ tests/model.test.js
@@ -5,10 +5,17 @@ const Model  = require('../model');
 
 // Model dovrebbe essere una classe (crea un'istanza della classe Model)
 test('Model dovrebbe essere una classe', () => {
-    const model = new Model('Dario', 34);
+    const model = new Model('Dario');
     expect(model).toBeInstanceOf(Model);
   });
-// L'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell'istanza
+
+
+// L'istanza di model dovrebbe richiedere il parametro obbligatorio del costruttore da usare in fase di creazione dell'istanza
+test('L\'istanza di model dovrebbe richiedere il parametro obbligatorio del costruttore da usare in fase di creazione dell\'istanza', () => {
+    expect(() => new Model()).toThrow();
+  });
+
+
 // L'istanza di model dovrebbe avere il metodo read
 // L'istanza di model dovrebbe avere il metodo add
 // read dovrebbe ritornare un array
