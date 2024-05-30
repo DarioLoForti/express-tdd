88ab8fefa90e584187ed08dd1bd3c33126ee6079
Dario Lo Forti <lofortidario@gmail.com>
2024-05-30 15:43:07 +0200

    Add Model class and test for class instance

--- tests/model.test.js
+++ tests/model.test.js
@@ -1,6 +1,13 @@
 // Bonus
 // Creiamo una classe Model la quale dovrà superare i seguenti test:
+const { test, expect } = require("@jest/globals");
+const Model  = require('../model');
+
 // Model dovrebbe essere una classe (crea un'istanza della classe Model)
+test('Model dovrebbe essere una classe', () => {
+    const model = new Model('Dario', 34);
+    expect(model).toBeInstanceOf(Model);
+  });
 // L'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell'istanza
 // L'istanza di model dovrebbe avere il metodo read
 // L'istanza di model dovrebbe avere il metodo add
